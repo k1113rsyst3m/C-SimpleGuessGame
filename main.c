1c509db8b95cf3acbba36656226f1868bc5893c3
k1113rsyst3m <117086020+k1113rsyst3m@users.noreply.github.com>
2022-10-31 16:31:17 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,39 +4,39 @@
 int main()
 {
     srand(time(NULL));
-    int guess_number = rand() % 11; // Случайное загаданное число от 0 до 10
-    int user_number; // Ваше число
-    int IsGame = 1; // Переменная определяющяя запущена ли игра
-    int IsWin = 0; // Переменная определяющяя победил ли игрок
-    int command = 0; // Команда, которую вы введёте после победы. 1 - продолжить игру 2 - закончить
+    int guess_number = rand() % 11;
+    int user_number; 
+    int IsGame = 1; 
+    int IsWin = 0; 
+    int command = 0; 
 
     while(IsGame == 1)
     {
        printf("Enter your number: ");
-       scanf("%d", &user_number); // отслежевание введёного числа
+       scanf("%d", &user_number);
 
-       if (user_number > guess_number) printf("Your number biggest\n"); // Если ваше число больше загаданного
-       if (user_number < guess_number) printf("Your number smallest\n"); // Если ваше число меньше загадонного
-       if (user_number == guess_number) // Если числа равно
+       if (user_number > guess_number) printf("Your number biggest\n"); 
+       if (user_number < guess_number) printf("Your number smallest\n"); 
+       if (user_number == guess_number) 
        {
-           // Выводим сообщение о том, что вы выиграли, даём переменной iswin значение 1
+           
            printf("You win! Do you want play again?\n1. Yes\n2. No\n>");
            IsWin = 1;
-           scanf("%d", &command); // Отслеживаем комманду
+           scanf("%d", &command); 
        }
        if((IsWin == 1) && (command == 1))
        {
-           // Обнуляем переменные
+           
            guess_number = rand() % 11;
-           user_number = -1; // -1 потому что число может быть 0
+           user_number = -1; 
            IsWin = 0;
            command = 0;
        }
        if((IsWin == 1) && (command == 2))
        {
-           IsGame = 0; // Выходим из игры
+           IsGame = 0; 
        }
 
-    } // цикл в котором будут происходить все действия игры
+    } 
     return 0;
 }

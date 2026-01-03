c63353fdbf5c83ff5fc319f899dc07e005f430d6
k111l3rsyst3m <ceiwbe@gmail.com>
2026-01-03 16:03:14 -0600

    Refactor and added unknown command handler

--- main.c
+++ main.c
@@ -1,43 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define MAX_GUESS_NUM 11
 
 int main()
 {
     srand(time(NULL));
-    int guess_number = rand() % 11;
-    int user_number; 
-    int IsGame = 1;
-    int command = 0; 
+    int guess_number = rand() % MAX_GUESS_NUM;
+    int user_number = 0; 
+    bool IsGame = true; 
 
-    while(IsGame == 1)
+    while(IsGame)
     {
+       int command = 0;
        printf("Enter your number: ");
        scanf("%d", &user_number);
 
-       if (user_number > guess_number) printf("Your number biggest\n"); 
-       if (user_number < guess_number) printf("Your number smallest\n"); 
+       if (user_number > guess_number) {
+	       printf("Your number biggest\n");
+	       continue;
+       }
+       if (user_number < guess_number) {
+	       printf("Your number smallest\n");
+	       continue;
+       }
        if (user_number == guess_number) 
        {
-           printf("You win!");
+           printf("You win! ");
            
        }
        unknow_command:
-       printf(" Do you want play again?\n1. Yes\n2. No\n>");
+       printf("Do you want play again?\n1. Yes\n2. No\n> ");
        scanf("%d", &command);
        
+       printf("command: %d\n", command);
+
        if(command == 1)
        {
-           
            guess_number = rand() % 11;
            user_number = -1; 
-           IsWin = 0;
            command = 0;
        }
-       if(command == 2)
+       else if(command == 2)
        {
-           IsGame = 0; 
+           IsGame = false; 
        }
        else {
+	   printf("Unknown command.\n");
+	   command = 0;
            goto unknow_command;
        }
     } 
